16f11115f0ea09c93e5f27fcebdc38f1a91bf494
maheshzh <maheshkm@blueehr.com>
2021-09-30 15:33:04 +0530

    error checking

--- index.php
+++ index.php
@@ -4,14 +4,11 @@
  
 <?php
  
-echo “<p>Example for __LINE__</p>”;  
-echo __LINE__ . "<br><br>";
- 
-echo “<p>Example for __FILE__</p>”;  
-echo __FILE__ . "<br><br>";
- 
-echo “<p>Example for __DIR__</p>”;  
-echo __DIR__ . "<br><br>";
+echo "Hello";
+
+?>
+
+
  
 </body>
 </html>
